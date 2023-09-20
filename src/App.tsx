bcb9839640bcec4817e73ec9fe75d4721cbad485
Benjamin <benjaminlikita3@gmail.com>
2023-09-20 20:36:45 +0100

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/home' element={<Home />}/>
+        <Route path='/' element={<Home />}/>
         <Route path='/about-me' element={<About />}/>
         <Route path='*' element={<Navigate to="/" />} />
       </Routes>
